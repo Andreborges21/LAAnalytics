c15e750cdff19a4cfafc48547ebda611a99879be
Andreborges21 <152662632+Andreborges21@users.noreply.github.com>
2024-09-16 20:55:45 -0300

    sdfsdfsdfsd
    dsfsdgdsfgsdggsd

--- LAAnalytics/src/usa/com/nba/atletas/Main.java
+++ LAAnalytics/src/usa/com/nba/atletas/Main.java
@@ -3,19 +3,25 @@ package usa.com.nba.atletas;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage palcoPrincipal) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("telaUm.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Scene cenario = new Scene(root);
+			cenario.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			palcoPrincipal.setScene(cenario);
+			
+			palcoPrincipal.setTitle("LAANALYTICS BRAZIL LCC");
+			
+			palcoPrincipal.getIcons().add(new Image(getClass().getResourceAsStream("/usa/com/nba/atletas/logo.png")));
+			
+			palcoPrincipal.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
